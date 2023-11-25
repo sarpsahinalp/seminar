9ae999144ea86b3bf5d4550c4397d9446f7ca6b8
sarps <sarpsahinalp.official@gmail.com>
2023-11-25 23:38:11 +0100

    added database and a frontend that sends post request to the server

---
+++ fingerprintdb/src/main/java/seminar/tum/fingerprintdb/FingerprintController.java
@@ -0,0 +1,43 @@
+package seminar.tum.fingerprintdb;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/api/fingerprints")
+@CrossOrigin(origins = "http://127.0.0.1:8081")
+public class FingerprintController {
+
+    private final FingerprintService fingerprintService;
+
+    @Autowired
+    public FingerprintController(FingerprintService fingerprintService) {
+        this.fingerprintService = fingerprintService;
+    }
+
+    @GetMapping
+    public List<Fingerprint> getAllFingerprints() {
+        return fingerprintService.getAllFingerprints();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Fingerprint> getFingerprintById(@PathVariable Long id) {
+        Optional<Fingerprint> fingerprint = fingerprintService.getFingerprintById(id);
+        return fingerprint.map(ResponseEntity::ok).orElse(ResponseEntity.notFound().build());
+    }
+
+    @PostMapping
+    public ResponseEntity<Fingerprint> createFingerprint(@RequestBody Fingerprint fingerprint) {
+        Fingerprint createdFingerprint = fingerprintService.saveFingerprint(fingerprint);
+        return ResponseEntity.ok(createdFingerprint);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteFingerprint(@PathVariable Long id) {
+        fingerprintService.deleteFingerprint(id);
+        return ResponseEntity.noContent().build();
+    }
+}
---
+++ fingerprintdb/src/main/java/seminar/tum/fingerprintdb/FingerprintDbApplication.java
@@ -0,0 +1,13 @@
+package seminar.tum.fingerprintdb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FingerprintDbApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FingerprintDbApplication.class, args);
+    }
+
+}
