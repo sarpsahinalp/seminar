e038d24e3ed0eea2ca2de5480efc20ad6e60b82c
sarps <sarpsahinalp.official@gmail.com>
2023-12-02 14:54:31 +0100

    deployment branch don't touch

--- fingerprintdb/src/main/java/seminar/tum/fingerprintdb/FingerprintRepository.java
+++
@@ -1,6 +0,0 @@
-package seminar.tum.fingerprintdb;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface FingerprintRepository extends JpaRepository<Fingerprint, Long> {
-}
